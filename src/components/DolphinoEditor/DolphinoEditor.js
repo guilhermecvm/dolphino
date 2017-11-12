b1ccd5a17ace681edf7c282de44d0d8c5669c099
Guilherme Miranda <guilhermecvm@users.noreply.github.com>
2017-11-12 21:29:52 +0000

    adding toolbar

---
+++ src/components/DolphinoEditor/DolphinoEditor.js
@@ -0,0 +1,79 @@
+import React from 'react'
+import { EditorState } from 'draft-js'
+import Editor from 'draft-js-plugins-editor'
+import {
+  BlockquoteButton,
+  BoldButton,
+  CodeBlockButton,
+  HeadlineOneButton,
+  HeadlineThreeButton,
+  HeadlineTwoButton,
+  ItalicButton,
+  UnderlineButton,
+} from 'draft-js-buttons'
+import createToolbarPlugin from 'draft-js-static-toolbar-plugin'
+import 'draft-js-static-toolbar-plugin/lib/plugin.css'
+
+const toolbarPlugin = createToolbarPlugin({
+  structure: [
+    BoldButton,
+    ItalicButton,
+    UnderlineButton,
+    BlockquoteButton,
+    CodeBlockButton,
+    HeadlineOneButton,
+    HeadlineTwoButton,
+    HeadlineThreeButton,
+  ],
+})
+const { Toolbar } = toolbarPlugin
+const plugins = [toolbarPlugin]
+
+const styles = {
+  editorWrapper: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    background: '#fbfbfb',
+  },
+  editor: {
+    flex: 1,
+    cursor: 'text',
+    padding: '1em',
+    overflow: 'scroll',
+  },
+}
+
+class DolphinoEditor extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { editorState: EditorState.createEmpty() }
+  }
+
+  componentDidMount() {
+    this.editor.focus()
+  }
+
+  onChange = editorState => this.setState({ editorState })
+
+  focus = () => this.editor.focus()
+
+  render() {
+    return (
+      <div style={styles.editorWrapper} onClick={this.focus}>
+        <div style={styles.editor}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            plugins={plugins}
+            ref={element => (this.editor = element)}
+          />
+        </div>
+        <Toolbar />
+      </div>
+    )
+  }
+}
+
+export default DolphinoEditor
---
+++ src/components/index.js
@@ -0,0 +1,1 @@
+export { default as DolphinoEditor } from './DolphinoEditor/DolphinoEditor'
